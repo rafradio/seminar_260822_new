64a6f5bed9dd428daecc2a5dfd0978d73b4bab33
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-30 22:37:31 +0300

    HW260822 entering rows and columns

---
+++ HW260822_tsk52/Addclass.cs
@@ -0,0 +1,92 @@
+using System;
+
+namespace ColumnAverageArray
+{
+    class InitialSettings : Program52
+    {
+        public int[,] array2Dimen;
+        public double[] averageForColumn;
+        public InitialSettings()
+        {
+            
+            this.Initsets(0);
+            this.averageForColumn = new double[this.arrayColumn];
+            this.array2Dimen = new int[this.arrayRow, this.arrayColumn];
+            this.InitArray();
+            this.EverageExec();
+        }
+        public void Initsets(int iType)
+        {
+            if (iType < 2)
+            {
+                string[] dataType = {"строка", "столбец"};
+                Console.Write($"Введите пожалуйста целое число для ({dataType[iType]}): ");
+                string enterUser = Console.ReadLine();
+                if (int.TryParse(enterUser, out int number))
+                {
+                    switch (iType)
+                    {
+                        case 0:
+                            this.arrayRow = number;
+                            break;
+                        case 1:
+                            this.arrayColumn = number;
+                            break;
+                    }
+                    iType += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число. Повторите!");
+                }
+                this.Initsets(iType);
+            }
+            return;
+        }
+        public void InitArray()
+        {
+            for (int i = 0; i < this.arrayRow; i++)
+            {
+                for (int j = 0; j < this.arrayColumn; j++)
+                {
+                    this.array2Dimen[i, j] = new Random().Next(this.arrayFirstNum, this.arrayUpperNum + 1);
+                }
+            }
+
+        }
+        public void EverageExec()
+        {
+            for (int j = 0; j < this.arrayColumn; j++) 
+            {
+                double sum = 0;
+                for (int i = 0; i < this.arrayRow; i++)
+                {
+                    sum += this.array2Dimen[i, j];
+                }
+                sum = sum / this.arrayRow;
+                this.averageForColumn[j] = Math.Round(sum, 2);
+            }
+        }
+        public void PrintExec() 
+        {
+            string s;
+            Console.WriteLine("Ваш массив:");
+            for (int i = 0; i < this.arrayRow; i++)
+            {
+                s = "";
+                for (int j = 0; j < this.arrayColumn; j++)
+                {
+                    s += String.Format("{0,-7}", this.array2Dimen[i, j]);
+                }
+                Console.Write($"\n{s}");
+            }
+            Console.WriteLine("\n\nВаши средние по столбцу:");
+            s = "";
+            for (int j = 0; j < this.arrayColumn; j++)
+            {
+                s += String.Format("{0,-7}", this.averageForColumn[j]);
+            }
+            Console.Write($"{s}");
+        }
+    }
+}
---
+++ HW260822_tsk52/ColumnAverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ColumnAverageArray
+{
+    class Program52
+    {
+        public int arrayFirstNum = 0;
+        public int arrayUpperNum = 10;
+        public int arrayRow;
+        public int arrayColumn;
+        static void Main(string[] args) 
+        {
+            Console.Clear();
+            InitialSettings init = new InitialSettings();
+            init.PrintExec();
+        }
+    }
+}
